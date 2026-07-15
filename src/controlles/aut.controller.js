60eaeb3a40c43626006d557f1c9701f57179369e
Mazhar <mazhartuhin8291@gmail.com>
2026-07-15 23:52:36 +0600

    ledger model

--- src/controlles/aut.controller.js
+++ src/controlles/aut.controller.js
@@ -47,69 +47,69 @@ async function userRegisterController(req, res) {
  * - POST /api/auth/login
   */
 
-// async function userLoginController(req, res) {
-//     const { email, password } = req.body
+async function userLoginController(req, res) {
+    const { email, password } = req.body
 
-//     const user = await userModel.findOne({ email }).select("+password")
+    const user = await userModel.findOne({ email }).select("+password")
 
-//     if (!user) {
-//         return res.status(401).json({
-//             message: "Email or password is INVALID"
-//         })
-//     }
+    if (!user) {
+        return res.status(401).json({
+            message: "Email or password is INVALID"
+        })
+    }
 
-//     const isValidPassword = await user.comparePassword(password)
+    const isValidPassword = await user.comparePassword(password)
 
-//     if (!isValidPassword) {
-//         return res.status(401).json({
-//             message: "Email or password is INVALID"
-//         })
-//     }
+    if (!isValidPassword) {
+        return res.status(401).json({
+            message: "Email or password is INVALID"
+        })
+    }
 
-//     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "3d" })
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "3d" })
 
-//     res.cookie("token", token)
+    res.cookie("token", token)
 
-//     res.status(200).json({
-//         user: {
-//             _id: user._id,
-//             email: user.email,
-//             name: user.name
-//         },
-//         token
-//     })
+    res.status(200).json({
+        user: {
+            _id: user._id,
+            email: user.email,
+            name: user.name
+        },
+        token
+    })
 
-// }
-// /**
-//  * - User Logout Controller
-//  * - POST /api/auth/logout
-//   */
-// async function userLogoutController(req, res) {
-//     const token = req.cookies.token || req.headers.authorization?.split(" ")[ 1 ]
+}
+/**
+ * - User Logout Controller
+ * - POST /api/auth/logout
+  */
+async function userLogoutController(req, res) {
+    const token = req.cookies.token || req.headers.authorization?.split(" ")[ 1 ]
 
-//     if (!token) {
-//         return res.status(200).json({
-//             message: "User logged out successfully"
-//         })
-//     }
-
-
-
-//     await tokenBlackListModel.create({
-//         token: token
-//     })
-
-//     res.clearCookie("token")
-
-//     res.status(200).json({
-//         message: "User logged out successfully"
-//     })
-
-// }
+    if (!token) {
+        return res.status(200).json({
+            message: "User logged out successfully"
+        })
+    }
 
 
-// module.exports = {
-//     userRegisterController,
-//     userLoginController,
-//     userLogoutController
-// }
+
+    await tokenBlackListModel.create({
+        token: token
+    })
+
+    res.clearCookie("token")
+
+    res.status(200).json({
+        message: "User logged out successfully"
+    })
+
+}
+
+
+module.exports = {
+    userRegisterController,
+    userLoginController,
+    userLogoutController
+}
